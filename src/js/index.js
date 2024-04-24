644fbd639edad61b3439a99029a4aa725c53c9f1
Hellen-Leite <hellenleitearaujo16@gmail.com>
2024-04-24 14:54:44 -0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+const botaoPlataforma= document.querySelector('.btn-plataforma')
+const listaPlataforma= document.querySelector('.btn-plataforma .plataformas')
+
+botaoPlataforma.addEventListener("click", function(){
+    botaoPlataforma.classList.toggle("ativo")
+})
