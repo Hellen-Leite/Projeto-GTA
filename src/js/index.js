63afda8a805a019b50b4e63b33c9498c1981ca24
Hellen-Leite <hellenleitearaujo16@gmail.com>
2024-04-24 15:01:14 -0300

    correção no JS

--- src/js/index.js
+++ src/js/index.js
@@ -2,5 +2,6 @@ const botaoPlataforma= document.querySelector('.btn-plataforma')
 const listaPlataforma= document.querySelector('.btn-plataforma .plataformas')
 
 botaoPlataforma.addEventListener("click", function(){
-    botaoPlataforma.classList.toggle("ativo")
+    console.log(listaPlataforma)
+    listaPlataforma.classList.toggle("ativo")
 })
